5def94509f45dd03db1c12ee26a9652b65908943
Rootingg <91620858+Rootingg@users.noreply.github.com>
2021-10-18 22:04:38 +0200

    Add files via upload

---
+++ sniffer.py
@@ -0,0 +1,17 @@
+#CREATED BY ROOTING
+
+from scapy.all import *
+
+def packet_callback(packet):
+    if packet[TCP].payload:
+        if packet[IP].dport == 80:
+            print("\n{}-> {}:{}:\n{}".format(packet[IP].src,
+                                            packet[IP].dst,
+                                            packet[IP].dport,
+                                            str(bytes(packet[TCP].payload)))
+)
+
+
+sniff(filter='tcp',prn=packet_callback,store=0,count=0)
+
+#CREATED BY ROOTING
